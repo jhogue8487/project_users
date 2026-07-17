8d9cbed29b9dae7159574a9f5e7a60ad132abe4b
jogm <jogm@gmail.com>
2026-07-17 17:19:59 -0500

    pruebas envio json y form con bodyparser incluido en express

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+
 import { configDotenv } from "dotenv";
 //import 'dotenv/config';//esto importa y ejecuta en una sola linea
 configDotenv();
@@ -6,10 +7,27 @@ configDotenv();
 const app = express();
 const port = process.env.PORT || 3030;
 
+//configuracion de envio de datos json o form
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("Hola mundo");
 });
 
+app.post("/form", (req, res) => {
+  const user = req.body.user || "nn";
+  const pass = req.body.pass || "utilizar";
+  res.json({
+    datos: { user, pass },
+  });
+});
+
+app.post("/datos", (req, res) => {
+  const datos = req.body;
+  res.json({ mensaje: "Datos recibidos", datos: datos });
+});
+
 app.listen(port, () => {
-  console.log(`http://localhost:${port}/`);
+  console.log(`SERVIDOR EN: http://localhost:${port}/`);
 });
