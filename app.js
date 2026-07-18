56492fc321741908c131a145f7cd5c9d4b87a288
jogm <jogm@gmail.com>
2026-07-17 21:01:06 -0500

    Creacion lista json usuarios, y endopoint listar usuarios

--- app.js
+++ app.js
@@ -1,31 +1,27 @@
 import express from "express";
-
-import { configDotenv } from "dotenv";
-//import 'dotenv/config';//esto importa y ejecuta en una sola linea
-configDotenv();
+import "dotenv/config"; //esto importa y ejecuta en una sola linea
+import fs from "fs";
+import path from "path";
 
 const app = express();
 const port = process.env.PORT || 3030;
 
+//ruta para lectura del archivo
+const ruta_archivo = path.join(import.meta.dirname, "usuarios.json");
 //configuracion de envio de datos json o form
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hola mundo");
-});
-
-app.post("/form", (req, res) => {
-  const user = req.body.user || "nn";
-  const pass = req.body.pass || "utilizar";
-  res.json({
-    datos: { user, pass },
+app.get("/usuarios", (req, res) => {
+  fs.readFile(ruta_archivo, "utf-8", (err, datos) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ error: "Error lectura archivo o conexion BD" });
+    }
+    const usuarios = JSON.parse(datos);
+    res.json(usuarios);
   });
-});
-
-app.post("/datos", (req, res) => {
-  const datos = req.body;
-  res.json({ mensaje: "Datos recibidos", datos: datos });
 });
 
 app.listen(port, () => {
