7fa263cd6c72b0afdb8808bbaf804df90765fdee
jogm <jogm@gmail.com>
2026-08-11 21:49:50 -0500

    creacion rama commonjs

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-import express from "express";
+const express = require("express");
+require("dontenv");
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hola mundo");
 });
 
+app.get("/aprendiz/:cc", (req, res) => {
+  const aprendizCc = req.params.cc || 111000111;
+  res.send(`EL aprendiz tiene el numero de idenrifiacion: ${aprendizCc}`);
+});
+
 app.listen(port, () => {
-  console.log("http://localhost:3000/");
+  console.log(`URL SERVIDOR: http://localhost:${PORT}`);
 });
