fae8dddb66a845f148ae5d7b2779603aa838e79a
jogm <jogm@gmail.com>
2026-08-17 13:42:31 -0500

    creado lista en json, modificado app.js endopoint

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const express = require("express");
-require("dontenv");
+require("dotenv");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const listaAprendices = require("./aprendices.json");
 
 app.get("/", (req, res) => {
-  res.send("Hola mundo");
+  res.send("Api de aprendices");
 });
 
-app.get("/aprendiz/:cc", (req, res) => {
+app.get("/api/aprendices", (req, res) => {
+  res
+    .status(200)
+    .json({ Mensaje: "Lista de aprendices", Lista: listaAprendices });
+});
+
+app.get(["/api/aprendiz", "/api/aprendiz/:cc"], (req, res) => {
   const aprendizCc = req.params.cc || 111000111;
-  res.send(`EL aprendiz tiene el numero de idenrifiacion: ${aprendizCc}`);
+  //res.send(`EL aprendiz tiene el numero de identifiacion: ${aprendizCc}`);
+  const aprendiz = listaAprendices.filter((a) => a.cc == aprendizCc);
+
+  res.json({ Aprendiz: aprendiz.length > 0 ? aprendiz : "sin datos" });
 });
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`URL SERVIDOR: http://localhost:${PORT}`);
 });
