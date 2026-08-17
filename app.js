e1e76592ce50846a4d48e18fe420a28d70f6dcbc
jogm <jogm@gmail.com>
2026-08-17 15:47:14 -0500

    endpoint listar todos y un aprendiz con archivo json

--- app.js
+++ app.js
@@ -1,28 +1,45 @@
 const express = require("express");
-require("dotenv");
+require("dotenv/config");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const listaAprendices = require("./aprendices.json");
+//datos para leer archivo
+//const listaAprendices = require("./aprendices.json");//manera sincrona
+const sisArchivo = require("fs");
+const ruta = require("path");
+const rutaArchivoJson = ruta.join(__dirname, "aprendices.json");
 
 app.get("/", (req, res) => {
   res.send("Api de aprendices");
 });
 
+//listar todos los aprendices
 app.get("/api/aprendices", (req, res) => {
-  res
-    .status(200)
-    .json({ Mensaje: "Lista de aprendices", Lista: listaAprendices });
+  sisArchivo.readFile(rutaArchivoJson, "utf-8", (err, datos) => {
+    if (err) {
+      res.status(500).json({ error: "Error conexion bd." });
+    }
+    const listaAprendices = JSON.parse(datos);
+    res.json({ "listado Aprendices": listaAprendices });
+  });
 });
 
+//listar un aprendiz
 app.get(["/api/aprendiz", "/api/aprendiz/:cc"], (req, res) => {
-  const aprendizCc = req.params.cc || 111000111;
+  const aprendizCc = req.params.cc || 10001;
   //res.send(`EL aprendiz tiene el numero de identifiacion: ${aprendizCc}`);
-  const aprendiz = listaAprendices.filter((a) => a.cc == aprendizCc);
-
-  res.json({ Aprendiz: aprendiz.length > 0 ? aprendiz : "sin datos" });
+  sisArchivo.readFile(rutaArchivoJson, "utf-8", (err, datos) => {
+    if (err) {
+      return res.status(500).json({ Error: "Error conexion bd." });
+    }
+    const aprendiz = JSON.parse(datos).filter((a) => a.cc == aprendizCc);
+    res.json({ Aprendiz: aprendiz.length > 0 ? aprendiz : "sin datos" });
+  });
 });
 
+//adicionar un aprendiz
+
+//escucha el puerto donde despliega el servidor
 app.listen(PORT, () => {
   console.log(`URL SERVIDOR: http://localhost:${PORT}`);
 });
